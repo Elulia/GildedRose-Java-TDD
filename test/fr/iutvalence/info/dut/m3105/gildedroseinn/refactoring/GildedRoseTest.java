7300105ceb7ce1a23ccad21655f030cc3533cb4d
combear <arthur.combe@iut-valence.fr>
2015-11-03 14:57:18 +0100

    green1

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -2,21 +2,17 @@ package fr.iutvalence.info.dut.m3105.gildedroseinn.refactoring;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class GildedRoseTest
 {
 	
-	@Before
-	public Item initialisationItemList()
-	{
-		return (new Item("+5 Dexterity Vest", 10, 20));
-	}
+	
+	private static final Item firstItem = (new Item("+5 Dexterity Vest", 10, 20));
+	
 	@Test
 	public void testSellinAndQualityDecrease()
 	{
-		Item firstItem = initialisationItemList();
 		GildedRose.updateItem(firstItem);
 		assertEquals(firstItem.getSellIn(),9);
 		assertEquals(firstItem.getQuality(),19);
