faa9925324bce6a48ae5dd25e516886a59f61a44
combear <arthur.combe@iut-valence.fr>
2015-11-03 15:04:20 +0100

    Red2

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -9,13 +9,22 @@ public class GildedRoseTest
 	
 	
 	private static final Item FIRST_ITEM = (new Item("+5 Dexterity Vest", 10, 20));
+	private static final Item SECOND_ITEM = (new Item("+5 Dexterity Vest", 0, 20));
 	
 	@Test
-	public void testSellinAndQualityDecrease()
+	public void testNoConditionSellinAndQualityDecrease()
 	{
 		GildedRose.updateItem(FIRST_ITEM);
 		assertEquals(FIRST_ITEM.getSellIn(),9);
 		assertEquals(FIRST_ITEM.getQuality(),19);
+	}
+	
+	@Test
+	public void testSellInInferiorToZeroSellinAndQualityDecrease()
+	{
+		GildedRose.updateItem(SECOND_ITEM);
+		assertEquals(SECOND_ITEM.getSellIn(),-1);
+		assertEquals(SECOND_ITEM.getQuality(),18);
 	}
 
 }
